c783a0ad77cfc643bd810148a482e4a0995ab7f0
YONGJIE <jspss96046@gmail.com>
2022-07-01 15:38:31 +0800

    ok

--- model.py
+++ model.py
@@ -9,20 +9,20 @@ from linebot.models import *
 
 
 def ask(): 
-    message = TextSendMessage(text="?????")
-    #import openai
-    #openai.api_key = "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"
-    # response = openai.Completion.create(
-    # model="text-curie-001",
-    # prompt="how old are you",
-    # temperature=0.7,
-    # max_tokens=256,
-    # top_p=1,
-    # frequency_penalty=0,
-    # presence_penalty=0
-    #                                     )
-    #story = response['choices'][0]['text'] 
     
+    import openai
+    openai.api_key = "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"
+    response = openai.Completion.create(
+    model="text-curie-001",
+    prompt="how old are you",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+                                        )
+    story = response['choices'][0]['text'] 
+    message = TextSendMessage(text=story)
     return message
     #print( str(story) )
 #ask("what are the symptom of heartdisease") 
