755dfa519f1ed547ab4b6497264892021d22b0be
YONGJIE <jspss96046@gmail.com>
2022-07-12 11:18:41 +0800

    tran

--- model.py
+++ model.py
@@ -4,19 +4,19 @@ from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
 
 def ask(q):
-    import openai
-    openai.api_key = "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"
-    response = openai.Completion.create(
-    model="text-curie-001",
-    prompt=q,
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-                                        )
-    story = response['choices'][0]['text'] 
-    message = TextSendMessage(text=story)
+    # import openai
+    # openai.api_key = "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"
+    # response = openai.Completion.create(
+    # model="text-curie-001",
+    # prompt=q,
+    # temperature=0.7,
+    # max_tokens=256,
+    # top_p=1,
+    # frequency_penalty=0,
+    # presence_penalty=0
+    #                                     )
+    # story = response['choices'][0]['text'] 
+    message = TextSendMessage(text=q)
     return message
     #return story
     #print( str(story) )
