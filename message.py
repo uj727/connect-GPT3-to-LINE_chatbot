9dbd0486648b3e652bdc6b4089decef0b10ad03c
YONGJIE <jspss96046@gmail.com>
2022-08-14 14:03:17 +0800

    OK

--- message.py
+++ message.py
@@ -16,11 +16,11 @@ def image_carousel_message1():
                         uri="https://haostore.fami.life/001971/"
                     )
                 ),
-                ImageCarouselColumn(
-                    image_url="https://youimg1.tripcdn.com/target/0106v120009g3u6op325B.jpg",
+                  ImageCarouselColumn(
+                    image_url="https://i.imgur.com/Np7eFyj.jpg",
                     action=URITemplateAction(
-                        label="吾舍。生活 Woosah-Life",
-                        uri="https://hiking.biji.co/index.php?q=trail&act=detail&id=598"
+                        label="可愛狗狗",
+                        uri="http://imgm.cnmo-img.com.cn/appimg/screenpic/big/674/673928.JPG"
                     )
                 ),
                 ImageCarouselColumn(
