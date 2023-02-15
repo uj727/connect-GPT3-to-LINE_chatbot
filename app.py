373e883b39c9522f56eecc4de3525bfc3736808b
uj727 <100131160+uj727@users.noreply.github.com>
2023-02-15 23:45:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,12 +49,9 @@ handler = WebhookHandler('')# Channel Secret
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
